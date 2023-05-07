4cff67050ecb71c011446ea7d809b3d4cbf9b4d0
Inryatt <inrymail@gmail.com>
2023-05-07 10:19:00 +0100

    initial barebones structure

---
+++ Project_1/Card_Crypt/README.md
@@ -0,0 +1,4 @@
+# Card Encryptor
+
+# Description:
+Receiving a plaintext card, encrypts it and exports the encrypted result.
---
+++ Project_1/Card_Crypt/crypt.cpp
@@ -0,0 +1,14 @@
+
+// given a n x m sized matrix of 3-digit ints, encrypt it
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+string* encrypt(string card[]){
+
+
+pass
+}
